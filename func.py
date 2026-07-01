1b685e0cae7ef6ebe08b1f760725e01cddb06484
SebastianLai <surregograciano@gmail.com>
2026-07-01 18:21:16 -0500

    second-commit

---
+++ func.py
@@ -0,0 +1,5 @@
+
+def greet(name): 
+  print(f'Hello, what are you doing {name}')
+
+greet('Sebastian')
--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 
 print('Hello World!!!!')
 
+items = ['Sebastian', 'Urrego', 18]
+
+for item in items: 
+  print(item)
